d71e8d3a98c3a427080195cc4c19c6aa553eb04d
Ahmad syaerozi <144976767+5520123003@users.noreply.github.com>
2023-11-13 10:06:13 +0800

    Add files via upload
    Hari ini saya belajar tentang data list atau array

---
+++ list.py
@@ -0,0 +1,58 @@
+nama = "yudis"
+print(nama)
+nama2 = ["indi","anggun","krtika","eka","silpi","istiana","lydia"]
+print(nama2)
+
+usia = [20,30,40,55,66]
+print(usia)
+
+jumlah_mahasiswa = len(nama2)
+print(jumlah_mahasiswa)
+
+status = [True,False,True,False,False]
+print(status)
+
+print(nama2[2])
+print(usia[4])
+print(usia[-1])
+nama2[len(nama2)-1]
+
+print('\n\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
